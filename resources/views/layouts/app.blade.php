4f8564c8bfabb07dd840240c4599686cf8d063e8
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-04-25 03:46:25 -0400

    First

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name') }}</title>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body class="antialiased font-sans bg-gray-900 text-gray-300">
+        <div id="app">
+            @yield('content')
+        </div>
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </body>
+</html>
