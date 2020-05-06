38e886d33603f7eeb6d1a4bbdc32aecf0aaf1112
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-05-06 00:26:08 -0400

    Prefix

--- routes/api.php
+++ routes/api.php
@@ -2,30 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'Api'], function () {
-    Route::group(['namespace' => 'Timeline'], function () {
-        Route::get('/timeline', 'TimelineController@index');
+Route::group(['prefix' => '/timeline', 'namespace' => 'Timeline'], function () {
+    Route::get('/', 'TimelineController@index');
+});
+
+Route::group(['prefix' => '/tweets', 'namespace' => 'Tweets'], function () {
+    Route::post('/', 'TweetController@store');
+
+    Route::group(['prefix' => '/{tweet}'], function () {
+        Route::post('/replies', 'TweetReplyController@store');
+
+        Route::post('/retweets', 'TweetRetweetController@store');
+        Route::delete('/retweets', 'TweetRetweetController@destroy');
+
+        Route::post('/quotes', 'TweetQuoteController@store');
+
+        Route::post('/likes', 'TweetLikeController@store');
+        Route::delete('/likes', 'TweetLikeController@destroy');
     });
+});
 
-    Route::group(['prefix' => '/tweets', 'namespace' => 'Tweets'], function () {
-        Route::post('/', 'TweetController@store');
-
-        Route::group(['prefix' => '/{tweet}'], function () {
-            Route::post('/replies', 'TweetReplyController@store');
-
-            Route::post('/retweets', 'TweetRetweetController@store');
-            Route::delete('/retweets', 'TweetRetweetController@destroy');
-
-            Route::post('/quotes', 'TweetQuoteController@store');
-
-            Route::post('/likes', 'TweetLikeController@store');
-            Route::delete('/likes', 'TweetLikeController@destroy');
-        });
-    });
-
-    Route::group(['prefix' => '/media', 'namespace' => 'Media'], function () {
-        Route::post('/', 'MediaController@store');
-        
-        Route::get('/types', 'MediaTypesController@index');
-    });
+Route::group(['prefix' => '/media', 'namespace' => 'Media'], function () {
+    Route::post('/', 'MediaController@store');
+    
+    Route::get('/types', 'MediaTypesController@index');
 });
