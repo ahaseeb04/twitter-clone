d56b50f14b8eec0e88b353c8f0aa9e69bcdbf935
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-06-02 14:33:58 -0400

    Use Inter var

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
             },
 
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans]
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans]
             },
 
             maxWidth: {
