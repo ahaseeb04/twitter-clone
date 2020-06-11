4d3e782e7a760cb129a67d0434f3dd49659603de
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-06-11 14:53:26 -0400

    Added header

---
+++ resources/js/components/app/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full inline-block p-4 border-b border-gray-800">
+        <span class="text-white font-semibold">{{ text }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            text: {
+                required: true,
+                type: String
+            }
+        }
+    }
+</script>
