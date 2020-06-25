30c8775167fe056ad954f75814c630ee0ce73fa1
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-06-25 00:53:13 -0400

    Fixed timestamps bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,7 +78,8 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(
             User::class, 'followers', 'user_id', 'following_id'
-        );
+        )
+            ->withTimestamps();
     }
 
     /**
@@ -90,7 +91,8 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(
             User::class, 'followers', 'following_id', 'user_id'
-        );
+        )
+            ->withTimestamps();
     }
 
     /**
